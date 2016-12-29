07900fdbc923a82bfee4c8aae0e890f314783cb7
Jose Flores <josefflores@users.noreply.github.com>
2016-12-28 19:44:27 -0500

    Create pi.setup.sh

---
+++ pi.setup.sh
@@ -0,0 +1,29 @@
+# Raspberry Pi Setup Script
+
+mkdir ~/Git
+
+# Remove Packages
+# hexchat, pidgin, thunderbird, transmission, you tube
+sudo apt-get remove plank minecraft-pi* shotwell* firefox scratch libreoffice-* atril* sonic-pi* brasero* cheese* rhythmbox* vlc*
+sudo apt-get clean
+sudo apt-get autoremove
+
+# Install Teamviewer
+wget http://download.teamviewer.com/download/linux/version_11x/teamviewer-host_armhf.deb ~/Downloads/teamviewer-host_armhf.deb
+dpkg -i ~/Downloads/teamviewer-host_armhf.deb
+sudo apt-get install -f
+rm -rf ~/Downloads/teamviewer-host_armhf.deb
+  
+# Install Chromium
+# https://www.raspberrypi.org/forums/viewtopic.php?t=121195
+wget -qO - http://bintray.com/user/downloadSubjectPublicKey?username=bintray | sudo apt-key add -
+echo "deb http://dl.bintray.com/kusti8/chromium-rpi jessie main" | sudo tee -a /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install chromium-browser rpi-youtube -y
+
+# Install nodejs
+wget http://node-arm.herokuapp.com/node_latest_armhf.deb ~/Downloads/node_latest_armhf.deb
+sudo dpkg -i ~/Downloads/node_latest_armhf.deb
+
+# Git
+sudo apt-get install git
